95265e7d6ae0c9533e96a99b15dd34a0c4970777
George Artavanis <ga6@verde.princeton.edu>
2023-07-14 16:37:22 -0400

    changed function to work for all ref_yamls

--- src/subsettools/datasets.py
+++ src/subsettools/datasets.py
@@ -1,11 +1,11 @@
 from importlib import resources
 
-def get_ref_yaml_path(grid):
-    if grid == "conus1":
-        filename = "C1_pfclm_transient_solid.yaml"
-    elif grid == "conus2":
-        filename = ""
-        
+def get_ref_yaml_path(grid, mode, input_file_type):    
+    if mode == "transient":
+        mode = "_pfclm_" + mode + "_"
+    else:
+        mode = "_pf_" + mode + "_"
+    filename = grid + mode + input_file_type + ".yaml"
     with resources.path("subsettools.ref_yamls", filename) as f:
         data_file_path = f
     return data_file_path
