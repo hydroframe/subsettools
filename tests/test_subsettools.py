aa598dba349fd3c095f499edcd821ae356fa6545
George Artavanis <ga6@verde.princeton.edu>
2023-07-24 12:39:54 -0400

    added first test

--- tests/test_subsettools.py
+++ tests/test_subsettools.py
@@ -1,1 +1,4 @@
-from subsettools import subsettools
+from subsettools import subsettools, datasets
+
+def test_get_conus_ij():
+    assert subsettools.get_conus_ij("15060202", "conus1") == [375, 239, 487, 329]
