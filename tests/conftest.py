cfea71e654bcd588a0c436e61dcdfecaf2f449c3
George Artavanis <ga6@verde.princeton.edu>
2023-08-04 14:25:01 -0400

    cleanup environment variable PARFLOW_DIR

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,16 @@ import os
 from parflow.tools.fs import get_absolute_path, mkdir, rm
 
 @pytest.fixture
-def setup():
+def setup_dir_structure():
     home = os.path.expanduser("~")
     base_dir = f"{home}/subsettools_test_output/"
     input_dir = base_dir + "inputs/"
     output_dir = base_dir + "outputs/"
+    temp_pf_dir = os.environ.get("PARFLOW_DIR")
     os.environ["PARFLOW_DIR"] = "/home/SHARED/software/parflow/3.10.0"
     correct_output_dir = get_absolute_path('tests/correct_output')
 
-    def setup_test(run_name):
+    def setup(run_name):
         static_write_dir = input_dir + run_name + "/static/"
         mkdir(static_write_dir)
         forcing_dir = input_dir + run_name + "/forcing/"
@@ -21,6 +22,11 @@ def setup():
         target_runscript = pf_out_dir + run_name + '.yaml'
         return  static_write_dir, forcing_dir, pf_out_dir, correct_output_dir, target_runscript
 
-    yield setup_test
-
+    yield setup
+    
+    if temp_pf_dir is not None:
+        os.environ["PARFLOW_DIR"]
+    else:
+        del os.environ["PARFLOW_DIR"]
+        
     rm(base_dir)
