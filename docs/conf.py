eefa90c8e225e71c53354c08eeb719a46b4377d3
gartavanis <32808713+gartavanis@users.noreply.github.com>
2023-09-07 10:27:40 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ author = "George Artavanis, Amanda Triplett"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
+    "myst-nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
