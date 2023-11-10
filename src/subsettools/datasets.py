d8e94d26145bac8f1f2afd959e6dec0482d3d7fa
George Artavanis <ga6@verde.princeton.edu>
2023-11-09 20:19:40 -0500

    add guards for get_template_runscript()

--- src/subsettools/datasets.py
+++ src/subsettools/datasets.py
@@ -17,10 +17,20 @@ def get_template_runscript(grid, mode, input_file_type, write_dir):
     Returns:
         str: Path to the template runscript.
     """
-    assert grid in ["conus1", "conus2"], "invalid grid provided"
-    assert mode in ["transient", "spinup"], "invalid mode"
-    assert input_file_type in ["box", "solid"], "invalid input file type"
-    assert os.path.isdir(write_dir), "write_dir must be a directory"
+    if not isinstance(grid, str):
+        raise TypeError("grid must be a string")
+    if not isinstance(mode, str):
+        raise TypeError("mode must be a string")
+    if not isinstance(input_file_type, str):
+        raise TypeError("input_file_type must be a string")
+    if grid not in ["conus1", "conus2"]:
+        raise ValueError("Supported grids are 'conus1' and 'conus2'")
+    if mode not in ["transient", "spinup"]:
+        raise ValueError("Supported modes are 'transient' and 'spinup'")
+    if input_file_type not in ["box", "solid"]:
+        raise ValueError("Supported input file types are 'box' and 'solid'")
+    if not os.path.isdir(write_dir):
+        raise FileNotFoundError("write_dir must be a valid directory") 
 
     if mode == "transient":
         mode = "_pfclm_" + mode + "_"
