80c8ed0e33872507280bcc45f37632b76228eab8
George Artavanis <ga6@verde.princeton.edu>
2024-05-14 23:19:13 -0400

    Format with black

--- tests/test_subsettools.py
+++ tests/test_subsettools.py
@@ -8,36 +8,60 @@ from parflow import Run
 
 
 @pytest.mark.parametrize(
-    "hucs, grid, result", [(["15060202"], "conus1", (375, 239, 487, 329)),
-                           (["140802"], "conus1", (572, 337, 797, 577)),
-                           (["14080201", "14080202", "14080203", "14080204", "14080205"], "conus1", (572, 337, 797, 577))]
+    "hucs, grid, result",
+    [
+        (["15060202"], "conus1", (375, 239, 487, 329)),
+        (["140802"], "conus1", (572, 337, 797, 577)),
+        (
+            ["14080201", "14080202", "14080203", "14080204", "14080205"],
+            "conus1",
+            (572, 337, 797, 577),
+        ),
+    ],
 )
 def test_define_huc_domain(hucs, grid, result):
     bounds, _ = st.define_huc_domain(hucs, grid)
     assert bounds == result
 
-    
+
 @pytest.mark.parametrize(
-    "hucs, grid", [(["01010001"], "conus1"),
-                   (["01010001"], "conus2"),
-                   (["03130003"], "conus1"),
-                   (["1710"], "conus1"),
-                   (["01010002", "01010001"], "conus1"),
-    ]
+    "hucs, grid",
+    [
+        (["01010001"], "conus1"),
+        (["01010001"], "conus2"),
+        (["03130003"], "conus1"),
+        (["1710"], "conus1"),
+        (["01010002", "01010001"], "conus1"),
+    ],
 )
 def test_define_huc_domain_errors(hucs, grid):
     """Check that a ValueError is raised if a HUC is not part of the grid."""
     with pytest.raises(ValueError) as e:
         st.define_huc_domain(hucs, grid)
 
-        
+
 @pytest.mark.parametrize(
     "latlon_bounds, grid, correct_bounds, correct_mask",
     [
-        ([[37.91, -91.43], [37.34, -90.63]], "conus1", (2285, 436, 2359, 496), np.ones((60, 74))),
-        ([[37.91, -91.43], [37.34, -90.63]], "conus2", (2684, 1403, 2758, 1461), np.ones((58, 74))),
-        ([[40.17, -124.27], [40.19, -124.25]], "conus2", (4, 2066, 7, 2069), np.array([[0, 0, 1], [0, 0, 1], [0, 1, 1]])),
-    ]
+        (
+            [[37.91, -91.43], [37.34, -90.63]],
+            "conus1",
+            (2285, 436, 2359, 496),
+            np.ones((60, 74)),
+        ),
+        (
+            [[37.91, -91.43], [37.34, -90.63]],
+            "conus2",
+            (2684, 1403, 2758, 1461),
+            np.ones((58, 74)),
+        ),
+        (
+            [[40.17, -124.27], [40.19, -124.25]],
+            "conus2",
+            (4, 2066, 7, 2069),
+            np.array([[0, 0, 1], [0, 0, 1], [0, 1, 1]]),
+        ),
+    ],
 )
 def test_define_latlon_domain(latlon_bounds, grid, correct_bounds, correct_mask):
     bounds, mask = st.define_latlon_domain(latlon_bounds, grid)
@@ -48,7 +72,11 @@ def test_define_latlon_domain(latlon_bounds, grid, correct_bounds, correct_mask)
 @pytest.mark.parametrize(
     "latlon_bounds, grid",
     [
-        pytest.param([[57.44, -107.33], [57.44, -107.33]], "conus2", id="lat/lon points outside the grid"),
+        pytest.param(
+            [[57.44, -107.33], [57.44, -107.33]],
+            "conus2",
+            id="lat/lon points outside the grid",
+        ),
     ],
 )
 def test_define_latlon_domain_errors(latlon_bounds, grid):
@@ -63,30 +91,52 @@ def test_write_mask_solid(set_parflow_dir, tmp_path):
     test_dir = tmp_path / "test"
     test_dir.mkdir()
     st.write_mask_solid(mask, grid, test_dir)
-    correct_output_dir = os.path.join(os.getcwd(), "tests", "correct_output", "conus1_upper_verde")
-    match, mismatch, errors = filecmp.cmpfiles(test_dir, correct_output_dir, ["mask.pfb", "solidfile.pfsol"], shallow=False)
+    correct_output_dir = os.path.join(
+        os.getcwd(), "tests", "correct_output", "conus1_upper_verde"
+    )
+    match, mismatch, errors = filecmp.cmpfiles(
+        test_dir, correct_output_dir, ["mask.pfb", "solidfile.pfsol"], shallow=False
+    )
     assert len(match) == 2 and len(mismatch) == 0 and len(errors) == 0
 
 
 def test_forcing_timezones(tmp_path):
     "Check if we get the correct forcing (temperature) in EST time."
-    utc = tmp_path / 'UTC_out'
+    utc = tmp_path / "UTC_out"
     utc.mkdir()
-    est = tmp_path / 'EST_out'
+    est = tmp_path / "EST_out"
     est.mkdir()
     ij_bounds = (375, 239, 487, 329)
-    grid = 'conus1'
-    start = '2005-10-03' 
-    dataset = 'NLDAS2'
-    st.subset_forcing(ij_bounds=ij_bounds, grid=grid, start=start, end='2005-10-05', dataset=dataset, write_dir=utc, time_zone='UTC')
-    st.subset_forcing(ij_bounds=ij_bounds, grid=grid, start=start, end='2005-10-04', dataset=dataset, write_dir=est, time_zone='EST')
+    grid = "conus1"
+    start = "2005-10-03"
+    dataset = "NLDAS2"
+    st.subset_forcing(
+        ij_bounds=ij_bounds,
+        grid=grid,
+        start=start,
+        end="2005-10-05",
+        dataset=dataset,
+        write_dir=utc,
+        time_zone="UTC",
+    )
+    st.subset_forcing(
+        ij_bounds=ij_bounds,
+        grid=grid,
+        start=start,
+        end="2005-10-04",
+        dataset=dataset,
+        write_dir=est,
+        time_zone="EST",
+    )
     utc_temp1 = read_pfb(os.path.join(utc, "NLDAS.Temp.000001_to_000024.pfb"))
     utc_temp2 = read_pfb(os.path.join(utc, "NLDAS.Temp.000025_to_000048.pfb"))
-    est_temp_correct = np.concatenate((utc_temp1[5:, :, :], utc_temp2[:5, :, :]), axis=0)
+    est_temp_correct = np.concatenate(
+        (utc_temp1[5:, :, :], utc_temp2[:5, :, :]), axis=0
+    )
     est_temp = read_pfb(os.path.join(est, "NLDAS.Temp.000001_to_000024.pfb"))
     assert np.array_equal(est_temp_correct, est_temp)
 
-    
+
 def test_edit_runscript_for_subset_1(tmp_path):
     """Check the edited fiedls of the runscript file."""
     test_dir = tmp_path / "test"
@@ -94,10 +144,8 @@ def test_edit_runscript_for_subset_1(tmp_path):
     runscript = st.get_template_runscript("conus1", "transient", "box", test_dir)
     forcing_dir = tmp_path / "forcing"
     forcing_dir.mkdir()
-    runscript = st.edit_runscript_for_subset((10, 10, 25, 25),
-                                                      runscript,
-                                                      runname="my_new_run",
-                                                      forcing_dir=str(forcing_dir)
+    runscript = st.edit_runscript_for_subset(
+        (10, 10, 25, 25), runscript, runname="my_new_run", forcing_dir=str(forcing_dir)
     )
     run = Run.from_definition(runscript)
     assert run.get_name() == "my_new_run"
@@ -106,7 +154,7 @@ def test_edit_runscript_for_subset_1(tmp_path):
     assert run.ComputationalGrid.NY == 15
     assert run.Geom.domain.Upper.X == 15000
     assert run.Geom.domain.Upper.Y == 15000
-    
+
 
 def test_edit_runscript_for_subset_2(tmp_path):
     """Check the runscript file in write_dir"""
@@ -118,10 +166,8 @@ def test_edit_runscript_for_subset_2(tmp_path):
     filename = os.path.basename(runscript)
     forcing_dir = tmp_path / "forcing"
     forcing_dir.mkdir()
-    runscript = st.edit_runscript_for_subset((10, 10, 25, 25),
-                                                      runscript,
-                                                      write_dir=write_dir,
-                                                      forcing_dir=str(forcing_dir)
+    runscript = st.edit_runscript_for_subset(
+        (10, 10, 25, 25), runscript, write_dir=write_dir, forcing_dir=str(forcing_dir)
     )
     assert runscript == os.path.join(write_dir, filename)
     assert os.path.exists(runscript)
@@ -140,12 +186,10 @@ def test_edit_runscript_for_subset_3(tmp_path):
     runscript = st.get_template_runscript("conus1", "transient", "box", test_dir)
     forcing_dir = os.path.join(tmp_path, "forcing")
     with pytest.raises(Exception) as e_info:
-        runscript = st.edit_runscript_for_subset((10, 10, 25, 25),
-                                                          runscript,
-                                                          runname="my_new_run",
-                                                          forcing_dir=forcing_dir
+        runscript = st.edit_runscript_for_subset(
+            (10, 10, 25, 25), runscript, runname="my_new_run", forcing_dir=forcing_dir
         )
-    
+
 
 def test_change_filename_values_1(tmp_path):
     """Check the edited fiedls of the runscript file."""
@@ -153,10 +197,9 @@ def test_change_filename_values_1(tmp_path):
     test_dir.mkdir()
     old_runscript = st.get_template_runscript("conus1", "transient", "box", test_dir)
     test_file = test_dir / "slope_x.pfb"
-    test_file.write_bytes(b'Binary file contents')    
-    new_runscript = st.change_filename_values(old_runscript,
-                                                       runname="my_new_run",
-                                                       slopex=str(test_file)
+    test_file.write_bytes(b"Binary file contents")
+    new_runscript = st.change_filename_values(
+        old_runscript, runname="my_new_run", slopex=str(test_file)
     )
     # check that both files exist in the folder:
     assert os.path.exists(old_runscript)
@@ -167,17 +210,15 @@ def test_change_filename_values_1(tmp_path):
     assert run.get_name() == "my_new_run"
     assert run.TopoSlopesX.FileName == str(test_file)
 
-    
+
 def test_change_filename_values_2(tmp_path):
     """Check that the file is replaced if write_dir==None and runname==None"""
     test_dir = tmp_path / "test"
     test_dir.mkdir()
     old_runscript = st.get_template_runscript("conus1", "transient", "box", test_dir)
     test_file = test_dir / "slope_x.pfb"
-    test_file.write_bytes(b'Binary file contents')
-    new_runscript = st.change_filename_values(old_runscript,
-                                                       slopex=str(test_file)
-    )
+    test_file.write_bytes(b"Binary file contents")
+    new_runscript = st.change_filename_values(old_runscript, slopex=str(test_file))
     # check that old file has been replaced:
     assert os.path.exists(new_runscript)
     assert old_runscript == new_runscript
@@ -190,7 +231,7 @@ def test_subset_press_init(tmp_path):
     """Check that the call succeeds when it fetches data for the beginning of WY 2003.
 
     subset_press_init was initially designed to fetch data one hour before midnight on
-    the date given. So in this case, it would look at 11pm on 2002-09-30, which does 
+    the date given. So in this case, it would look at 11pm on 2002-09-30, which does
     not exist.
     """
     test_dir = tmp_path / "test"
@@ -201,4 +242,6 @@ def test_subset_press_init(tmp_path):
         date="2002-10-01",
         write_dir=test_dir,
     )
-    assert filename == os.path.join(test_dir, "conus1_baseline_mod_2002.10.01:00.00.00_UTC0_press.pfb")
+    assert filename == os.path.join(
+        test_dir, "conus1_baseline_mod_2002.10.01:00.00.00_UTC0_press.pfb"
+    )
