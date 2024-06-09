6ded22fa1b61eb1a8e0bbb7f37bd42d015103707
gartavanis <gartavanis@users.noreply.github.com>
2024-06-09 12:19:43 -0400

    Add hf.register_api_pin call to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 import pytest
 from parflow.tools.fs import mkdir, rm
+import hf_hydrodata as hf
 
+hf.register_api_pin("email", "hf_pin")
 
 @pytest.fixture(scope="session")
 def set_parflow_dir():
