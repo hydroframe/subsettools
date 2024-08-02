d56c5ac0c4c14d4baa4f254e0ea84a82e6825a6e
egwilborn <egwilborn@gmail.com>
2024-08-02 14:02:57 -0400

    adds test for drv_clm file data

--- tests/test_clm.py
+++ tests/test_clm.py
@@ -48,6 +48,48 @@ def test_config_clm_data_vegm(mock_hf_get_file, mock_hf_data, tmp_path):
     with open(f"{write_dir}/drv_vegm.dat") as my_file:
         file_lines = my_file.readlines()
     assert file_lines[2] == '1 1 1.000000 1.000000 1.00 1.00 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n'
-    
+    assert len(file_lines) == 3
 
-    
+
+@pytest.mark.parametrize(
+    "start, end, expected_start, expected_end",
+    [
+        pytest.param("2005-10-01","2005-10-06", "2005101", "2005105", id="time window of one year"),
+        pytest.param("2005-10-01", "2006-10-01", "2005101", "2006930", id="time window of 5 days")
+    ],
+)
+def test_config_clm_data_drv_clm(start, end, expected_start, expected_end, mock_hf_get_file, mock_hf_data, tmp_path):
+    write_dir = tmp_path / "write"
+    write_dir.mkdir()
+    file_paths = st.config_clm(
+    ij_bounds=(0,0,10,10),
+    start=start,
+    end=end,
+    dataset="conus1_baseline_mod",
+    write_dir=write_dir
+    )
+    # open file and save lines
+    with open(f"{write_dir}/drv_clmin.dat") as my_file:
+        file_lines = my_file.readlines()
+    # start and end dates are written as three different lines
+    # so I use this function to pull out the numbers from each line and put them together in an array
+    start_numbers = []
+    end_numbers = []
+    def pull_numbers(line_no, my_list):
+        line=file_lines[line_no]
+        stripped = line.replace(" ", "")
+        for char in stripped:
+            if char.isdigit():
+                my_list.append(char)
+    # pull out the start date
+    pull_numbers(60, start_numbers)
+    pull_numbers(59, start_numbers)
+    pull_numbers(58, start_numbers)
+    # pull out the end date
+    pull_numbers(67, end_numbers)
+    pull_numbers(66, end_numbers)
+    pull_numbers(65, end_numbers)
+
+    # check that it matches up with provided expected values
+    assert expected_start == "".join(start_numbers)
+    assert expected_end == "".join(end_numbers)
