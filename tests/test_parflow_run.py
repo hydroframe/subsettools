2a6fbede8c2d090c561b156138d4413143f638b4
gartavanis <gartavanis@users.noreply.github.com>
2024-08-30 13:45:05 -0400

    Refactor pytest fixture

--- tests/test_parflow_run.py
+++ tests/test_parflow_run.py
@@ -114,26 +114,44 @@ def test_change_filename_values_2(tmp_path):
 
 @pytest.fixture
 def setup_dummy_run(tmp_path):
-    old_dir = tmp_path / "old"
-    old_dir.mkdir()
-    slope_x = old_dir / "slope_x.pfb"
-    slope_x.write_text("Dummy slope_x input file")
-    slope_y = old_dir / "slope_y.pfb"
-    slope_y.write_text("Dummy slope_y input file")
-    ic_pressure = old_dir / "old_ic_pressure.pfb"
-    ic_pressure.write_text("Dummy ic_pressure input file")
-    run = Run("dummy_run")
-    run.TopoSlopesX.FileName = os.path.basename(slope_x)
-    run.TopoSlopesY.FileName = os.path.basename(slope_y)
-    run.GeomInput.Names = "domain_input"
-    run.GeomInput.domain_input.InputType = "Box"
-    run.GeomInput.domain_input.GeomName = "domain"
-    run.Geom.domain.ICPressure.FileName = os.path.basename(ic_pressure)
-    _create_nc_output(old_dir)
-    _create_pfb_output(old_dir)
-    _write_clm_restart_files(old_dir)
-    runscript_path, _ = run.write(file_format="yaml", working_directory=old_dir)
-    return runscript_path
+    def setup(clm_on=False):
+        old_dir = tmp_path / "old"
+        old_dir.mkdir()
+        slope_x = old_dir / "slope_x.pfb"
+        slope_x.write_text("Dummy slope_x input file")
+        slope_y = old_dir / "slope_y.pfb"
+        slope_y.write_text("Dummy slope_y input file")
+        ic_pressure = old_dir / "old_ic_pressure.pfb"
+        ic_pressure.write_text("Dummy ic_pressure input file")
+        run = Run("dummy_run")
+        run.TopoSlopesX.FileName = os.path.basename(slope_x)
+        run.TopoSlopesY.FileName = os.path.basename(slope_y)
+        run.GeomInput.Names = "domain_input"
+        run.GeomInput.domain_input.InputType = "Box"
+        run.GeomInput.domain_input.GeomName = "domain"
+        run.Geom.domain.ICPressure.FileName = os.path.basename(ic_pressure)
+        _create_nc_output(old_dir)
+        _create_pfb_output(old_dir)
+        if clm_on:
+            run.Solver.LSM = "CLM"
+            _write_clm_restart_files(old_dir)
+            _write_clm_driver_files(old_dir)
+        runscript_path, _ = run.write(file_format="yaml", working_directory=old_dir)
+        return runscript_path
+
+    return setup
+
+
+def _write_clm_driver_files(working_directory):
+    file_path = os.path.join(working_directory, "drv_vegm.dat")
+    with open(file_path, "w") as file:
+        file.write("Dummy vegm file.")
+    file_path = os.path.join(working_directory, "drv_vegp.dat")
+    with open(file_path, "w") as file:
+        file.write("Dummy vegp file.")
+    # Copy real drv_clmin.dat for testing.
+    file_path = os.path.join("tests", "correct_output", "drv_clmin.dat")
+    shutil.copy(file_path, working_directory)
 
 
 def _write_clm_restart_files(working_directory):
@@ -178,7 +196,7 @@ def _create_pfb_output(working_directory):
 
 
 def test_restart_run_new_directory(setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     new_dir = tmp_path / "new"
     assert not os.path.isdir(new_dir)
     st.restart_run(runscript_path=old_runscript, new_dir=new_dir)
@@ -186,7 +204,7 @@ def test_restart_run_new_directory(setup_dummy_run, tmp_path):
 
 
 def test_restart_run_valid_runscript(setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     new_dir = tmp_path / "new"
     new_runscript = st.restart_run(
         runscript_path=old_runscript,
@@ -200,7 +218,7 @@ def test_restart_run_valid_runscript(setup_dummy_run, tmp_path):
 
 @pytest.mark.parametrize("dir_name", [None, "new"])
 def test_restart_run_runscript_path(dir_name, setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     new_dir = None if dir_name is None else tmp_path / dir_name
     new_runscript = st.restart_run(
         runscript_path=old_runscript,
@@ -214,7 +232,7 @@ def test_restart_run_runscript_path(dir_name, setup_dummy_run, tmp_path):
 
 @pytest.mark.parametrize("runname", [None, "new_run"])
 def test_restart_run_filename(runname, setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     new_runscript = st.restart_run(
         runscript_path=old_runscript,
         runname=runname,
@@ -224,7 +242,7 @@ def test_restart_run_filename(runname, setup_dummy_run, tmp_path):
 
 
 def test_restart_run_copy_inputs(setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     new_dir = tmp_path / "new"
     st.restart_run(runscript_path=old_runscript, new_dir=new_dir)
     assert "slope_x.pfb" in os.listdir(new_dir)
@@ -233,7 +251,7 @@ def test_restart_run_copy_inputs(setup_dummy_run, tmp_path):
 
 
 def test_restart_run_forcing_directory(setup_dummy_run):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     runscript_path = st.restart_run(
         runscript_path=old_runscript,
         forcing_dir="forcing",
@@ -247,16 +265,10 @@ def test_restart_run_forcing_directory(setup_dummy_run):
     [("netcdf", False), ("netcdf", True), ("pfb", False), ("pfb", True)],
 )
 def test_restart_run_initial_pressure(output_type, clm_on, setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
-    run = Run.from_definition(old_runscript)
-    if clm_on:
-        run.Solver.LSM = "CLM"
-    runscript_path, _ = run.write(
-        file_format="yaml", working_directory=os.path.dirname(old_runscript)
-    )
+    old_runscript = setup_dummy_run(clm_on)
     new_dir = tmp_path / "new"
     _ = st.restart_run(
-        runscript_path=runscript_path,
+        runscript_path=old_runscript,
         new_dir=new_dir,
         output_type=output_type,
     )
@@ -277,15 +289,10 @@ def test_restart_run_initial_pressure(output_type, clm_on, setup_dummy_run, tmp_
 def test_restart_run_restart_timestep(
     clm_on, create_new_dir, result, setup_dummy_run, tmp_path
 ):
-    old_runscript = setup_dummy_run
-    run = Run.from_definition(old_runscript)
-    run.Solver.LSM = "CLM" if clm_on else None
+    old_runscript = setup_dummy_run(clm_on)
     new_dir = (tmp_path / "new") if create_new_dir else None
-    runscript_path, _ = run.write(
-        file_format="yaml", working_directory=os.path.dirname(old_runscript)
-    )
     new_runscript = st.restart_run(
-        runscript_path=runscript_path,
+        runscript_path=old_runscript,
         new_dir=new_dir,
     )
     run = Run.from_definition(new_runscript)
@@ -297,7 +304,7 @@ def test_restart_run_restart_timestep(
 
 
 def test_restart_run_stoptime(setup_dummy_run):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     stop_time = _NUM_TIMESTEPS + 24
     new_runscript = st.restart_run(runscript_path=old_runscript, stop_time=stop_time)
     run = Run.from_definition(new_runscript)
@@ -305,33 +312,23 @@ def test_restart_run_stoptime(setup_dummy_run):
 
 
 def test_restart_run_stoptime_errors(setup_dummy_run):
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     stop_time = _NUM_TIMESTEPS - 2
     with pytest.raises(ValueError):
         st.restart_run(runscript_path=old_runscript, stop_time=stop_time)
 
 
 def test_restart_run_copy_restart_files(setup_dummy_run, tmp_path):
-    old_runscript = setup_dummy_run
-    run = Run.from_definition(old_runscript)
-    run.Solver.LSM = "CLM"
-    runscript_path, _ = run.write(
-        file_format="yaml", working_directory=os.path.dirname(old_runscript)
-    )
+    old_runscript = setup_dummy_run(clm_on=True)
     new_dir = tmp_path / "new"
-    new_runscript = st.restart_run(runscript_path=runscript_path, new_dir=new_dir)
+    new_runscript = st.restart_run(runscript_path=old_runscript, new_dir=new_dir)
     assert os.path.exists(os.path.join(new_dir, "clm.rst.00000.0"))
 
 
 def test_restart_run_rename_clm_restart_files(setup_dummy_run):
-    old_runscript = setup_dummy_run
-    run = Run.from_definition(old_runscript)
-    run.Solver.LSM = "CLM"
+    old_runscript = setup_dummy_run(clm_on=True)
     working_directory = os.path.dirname(old_runscript)
-    runscript_path, _ = run.write(
-        file_format="yaml", working_directory=working_directory,
-    )
-    new_runscript = st.restart_run(runscript_path=runscript_path)
+    new_runscript = st.restart_run(runscript_path=old_runscript)
     clm_restart = _NUM_TIMESTEPS - _NUM_TIMESTEPS % 24
     clm_restart = str(clm_restart).rjust(5, '0')
     assert os.path.exists(os.path.join(working_directory, f"clm.rst.{clm_restart}.0"))
@@ -348,7 +345,7 @@ def test_restart_run_rename_clm_restart_files(setup_dummy_run):
 )
 def test_restart_run_drvclm(setup_dummy_run, stop_time, end_day, end_hour):
     stop_time = _NUM_TIMESTEPS + 1
-    old_runscript = setup_dummy_run
+    old_runscript = setup_dummy_run()
     working_directory = os.path.dirname(old_runscript)
     file_path = os.path.join("tests", "correct_output", "drv_clmin.dat")
     shutil.copy(file_path, working_directory)
@@ -362,3 +359,16 @@ def test_restart_run_drvclm(setup_dummy_run, stop_time, end_day, end_hour):
     assert startcode == 1
     assert start_hour == 0
     assert (eda, ehr) == (end_day, end_hour)
+
+
+def test_restart_run_copy_clm_files(setup_dummy_run, tmp_path):
+    old_runscript = setup_dummy_run()
+    run = Run.from_definition(old_runscript)
+    run.Solver.LSM = "CLM"
+    runscript_path, _ = run.write(file_format="yaml", working_directory=os.path.dirname(old_runscript))
+    new_dir = tmp_path / "new"
+    _ = st.restart_run(runscript_path=runscript_path, new_dir=new_dir)
+    assert os.path.exists(os.path.join(new_dir, "drv_clmin.dat"))
+    assert os.path.exists(os.path.join(new_dir, "drv_vegm.dat"))
+    assert os.path.exists(os.path.join(new_dir, "drv_vegp.dat"))
+    
