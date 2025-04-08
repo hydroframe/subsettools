6b18c69db20d3fa5c8a7729ed2db698aff2bf507
Amy Defnet <amydefnet@princeton.edu>
2025-04-08 16:23:54 -0400

    add comments, add option to write to disk

--- tests/test_clm.py
+++ tests/test_clm.py
@@ -1,10 +1,8 @@
 """Tests for clm.py module."""
-import pytest
 import numpy as np
 import subsettools as st
 
-
 def test_vegm_to_land_cover():
-    """Test vegm_to_land_cover conversion function."""
+    """Test vegm_to_land_cover conversion function for a small test domain."""
     test_land_cover = st.vegm_to_land_cover("tests/correct_output/test_vegm.dat")
     assert np.array_equal(test_land_cover, [[13., 6.], [18., 1.]])
